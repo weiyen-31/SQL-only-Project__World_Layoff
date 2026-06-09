7b026bd91f85f3a4f32e07861e2df9f57ebfa276
weiyen-31 <nick0531hwy@gmail.com>
2026-06-09 17:52:17 +0800

    Update and rename World_Layoff to Analysis 1: Monthly Layoff Hotspots.sql

---
+++ Analysis 1: Monthly Layoff Hotspots.sql
@@ -0,0 +1,17 @@
+WITH monthly_combo AS (
+    	SELECT DATE_FORMAT(date, '%Y-%m') AS month,
+        		country,
+        		industry,
+        		SUM(total_laid_off) AS total_laid_off
+   	FROM layoffs_staging2
+    	GROUP BY month, country, industry
+ 	)
+SELECT *
+FROM (
+    	SELECT *,
+           		DENSE_RANK() OVER (PARTITION BY month ORDER BY  
+total_laid_off DESC) AS rnk
+    	FROM monthly_combo
+ 	) t
+WHERE rnk <=3;
+
