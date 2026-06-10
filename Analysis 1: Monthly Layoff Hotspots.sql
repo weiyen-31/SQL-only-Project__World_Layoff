99009094530bb37a25d0c1b1599267dd5893ac56
weiyen-31 <nick0531hwy@gmail.com>
2026-06-10 16:19:36 +0800

    Update Analysis 1: Monthly Layoff Hotspots.sql

--- Analysis 1: Monthly Layoff Hotspots.sql
+++ Analysis 1: Monthly Layoff Hotspots.sql
@@ -1,6 +1,6 @@
 WITH monthly_combo AS (
     	SELECT DATE_FORMAT(date, '%Y-%m') AS month,
-        		country,
+        	    country,
         		industry,
         		SUM(total_laid_off) AS total_laid_off
    	FROM layoffs_staging2
@@ -10,7 +10,7 @@ SELECT *
 FROM (
     	SELECT *,
            		DENSE_RANK() OVER (PARTITION BY month ORDER BY  
-total_laid_off DESC) AS rnk
+                total_laid_off DESC) AS rnk
     	FROM monthly_combo
  	) t
 WHERE rnk <=3;
